38c66a5d6ea2e299ffa491a88d258f28ead9fddc
Deniz Doga Yanar <dyanar@k1m20s01.42istanbul.com.tr>
2026-04-03 17:21:45 +0300

    fix memory leaks

--- src/parser/input_validate.c
+++ src/parser/input_validate.c
@@ -1,9 +1,22 @@
 #include "pushswap.h"
 #include "libft.h"
 
+static		void free_argv(char **argv)
+{
+	for (int i = 0; argv[i] != NULL; i++)
+		free(argv[i]);
+	free(argv);
+}
+
 static int	is_valid_flag(char *s)
 {
 	if (ft_strncmp(s, "--simple", 8) == 0)
+		return (1);
+	else if (ft_strncmp(s, "--medium", 8) == 0)
+		return (1);
+	else if (ft_strncmp(s, "--complex", 9) == 0)
+		return (1);
+	else if (ft_strncmp(s, "--adaptive", 10) == 0)
 		return (1);
 	return (0);
 }
@@ -11,14 +24,20 @@ static int	is_valid_flag(char *s)
 static int	is_valid_number(char *s)
 {
 	char	**argv;
+	char	**tmp_argv;
 
 	argv = ft_split(s, ' ');
+	tmp_argv = argv;
 	while (*argv)
 	{
 		if(!ft_isdigit(**argv))
+		{
+			free_argv(tmp_argv);
 			return (0);
+		}
 		argv++;
 	}
+	free_argv(tmp_argv);
 	return (1);
 }
 int	is_input_valid(int argc, char **argv)
--- tests/test_parser.c
+++ tests/test_parser.c
@@ -9,9 +9,13 @@ static		void free_argv(int argc, char **argv)
 	free(argv);
 }
 
-static int	__is_input_valid(int argc, char *input, char delim)
+static int	__is_input_valid(char *input, char delim)
 {
-		char **argv = ft_split(input, delim);
+		int		argc;
+		char	**argv;
+
+		argv = ft_split(input, delim);
+		for (argc=0; argv[argc]; argc++);
 		int res = is_input_valid(argc, argv);
 		free_argv(argc, argv);
 		return (res);
@@ -19,23 +23,32 @@ static int	__is_input_valid(int argc, char *input, char delim)
 
 int	test_input_validation()
 {
-	TEST(__is_input_valid(1, "./push_swap", ' '),
+	TEST(__is_input_valid("./push_swap", ' '),
 		 0, "given argc=1 expected 0 but returned 1");
 
-	TEST(__is_input_valid(6, "./push_swap 1 2 3 4 5", ' '),
+	TEST(__is_input_valid("./push_swap 1 2 3 4 5", ' '),
 		 1, "given ./push_swap 1 2 3 4 5 expected 1 but returned 0");
 
-	TEST(__is_input_valid(2, "./push_swap+1 2 3 4 5", '+'),
+	TEST(__is_input_valid("./push_swap+1 2 3 4 5", '+'),
 		 1, "given ./push_swap '1 2 3 4 5' expected 1 but returned 0");
 
-	TEST(__is_input_valid(2, "./push_swap invalid", ' '),
+	TEST(__is_input_valid("./push_swap invalid", ' '),
 		 0, "given arg without \"--\" prefix, expected 0, but returned 1");
 
-	TEST(__is_input_valid(2, "./push_swap --invalid", ' '),
+	TEST(__is_input_valid("./push_swap --invalid", ' '),
 		 0, "given --invalid flag, expected 0, but returned 1");
 
-	TEST(__is_input_valid(3, "./push_swap --simple 1 2 3", ' '),
+	TEST(__is_input_valid("./push_swap --simple 1 2 3", ' '),
 		 1, "given \"--simple 1 2 3\", expected 1, but returned 0");
+
+	TEST(__is_input_valid("./push_swap --medium 1 2 3", ' '),
+		 1, "given \"--medium 1 2 3\", expected 1, but returned 0");
+
+	TEST(__is_input_valid("./push_swap --complex 1 2 3", ' '),
+		 1, "given \"--complex 1 2 3\", expected 1, but returned 0");
+
+	TEST(__is_input_valid("./push_swap --adaptive 1 2 3", ' '),
+		 1, "given \"--adaptive 1 2 3\", expected 1, but returned 0");
 
 	return (1);
 }
