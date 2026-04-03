525ce8910e157cf69e24272b4738c1aa396cb640
Deniz Doga Yanar <dyanar@k1m20s01.42istanbul.com.tr>
2026-04-03 14:05:02 +0300

    add libft into tester

--- tests/test_parser.c
+++ tests/test_parser.c
@@ -1,10 +1,12 @@
 #include "unit.h"
 #include "pushswap.h"
+#include "libft.h"
 
 int	test_input_validation()
 {
-	char **argv = (char **)malloc(sizeof(char **));
-	return (is_input_valid(argv));
+	char **argv = ft_split("push_swap 1 2 3 4 5", ' ');
+	TEST(is_input_valid(argv), 1);
+	return (0);
 }
 
 REGISTER_TEST(test_input_validation);
