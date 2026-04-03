63aa13e3232408000076bcc9e3813f45abcead55
Deniz Doga Yanar <dyanar@k1m20s01.42istanbul.com.tr>
2026-04-03 15:34:53 +0300

    free ft_split memory in parser test

--- tests/test_parser.c
+++ tests/test_parser.c
@@ -2,17 +2,28 @@
 #include "pushswap.h"
 #include "libft.h"
 
+static		void free_argv(int argc, char **argv)
+{
+	for (int i = 0; i<argc; i++)
+		free(argv[i]);
+	free(argv);
+}
+
+static int	__is_input_valid(int argc, char *input)
+{
+		char **argv = ft_split(input, ' ');
+		int res = is_input_valid(argc, argv);
+		free_argv(argc, argv);
+		return (res);
+}
+
 int	test_input_validation()
 {
-	{
-		char **argv = ft_split("push_swap", ' ');
-		TEST(is_input_valid(1, argv), 0, "given argc=1 expected 0 but returned 1");
-	}
+	TEST(__is_input_valid(1, "push_swap"),
+		 0, "given argc=1 expected 0 but returned 1");
 
-	{
-		char **argv = ft_split("push_swap 1 2 3 4 5", ' ');
-		TEST(is_input_valid(6, argv), 1, "given only numbers as separate args expected 1 but returned 0");
-	}
+	TEST(__is_input_valid(6, "push_swap 1 2 3 4 5"),
+		 1, "given only numbers as separate args expected 1 but returned 0");
 	return (1);
 }
 
