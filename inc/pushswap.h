8d699bd77a3a1f158cec8f3449160d79908e02d1
Deniz Doga Yanar <dyanar@k1m24s02.42istanbul.com.tr>
2026-04-04 17:07:33 +0300

    add s_ctx

--- inc/pushswap.h
+++ inc/pushswap.h
@@ -1,7 +1,20 @@
 #ifndef PUSHSWAP_H
 # define PUSHSWAP_H
 
+# include "libft.h"
+
+typedef struct s_ctx
+{
+	t_list	*num_lst;
+	void	(*sort)(t_list **);
+	int		bench;
+}	t_ctx;
+
+
 int		is_input_valid(int argc, char **argv);
+t_ctx	*parse(int argc, char **argv);
+		
 void	free_argv(char **argv);
+
 
 #endif
