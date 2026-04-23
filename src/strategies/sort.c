882768bac7f8e16a5b051da4d2b673768ccd5bea
eylulzehrakablan <eylulzehrakablan@gmail.com>
2026-04-23 04:20:44 +0300

    removed medium_sort() in sort.c

--- src/strategies/sort.c
+++ src/strategies/sort.c
@@ -18,9 +18,3 @@ void	simple_sort(t_stack **stack_a, t_stack **stack_b, t_ctx *ctx)
 	(void)stack_b;
 	(void)ctx;
 }
-void	medium_sort(t_stack **stack_a, t_stack **stack_b, t_ctx *ctx)
-{
-	(void)stack_a;
-	(void)stack_b;
-	(void)ctx;
-}
