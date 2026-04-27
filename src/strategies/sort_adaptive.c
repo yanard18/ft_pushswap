b8fb663caabddad563e442a6421de6f1c2795aaa
Deniz Doga Yanar <dyanar@k1m06s06.42istanbul.com.tr>
2026-04-27 19:07:51 +0300

    fix: norm sort_adaptive.c

--- src/strategies/sort_adaptive.c
+++ src/strategies/sort_adaptive.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_adaptive.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dyanar <dyanar@student.42istanbul.com.tr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/27 19:07:15 by dyanar            #+#    #+#             */
+/*   Updated: 2026/04/27 19:07:39 by dyanar           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-void set_adaptive_string(t_ctx *ctx, float disorder)
+void	set_adaptive_string(t_ctx *ctx, float disorder)
 {
 	if (disorder < 0.2)
 		ctx->benchmark.strategy = "Adaptive / O(n²)";
@@ -12,14 +24,12 @@ void set_adaptive_string(t_ctx *ctx, float disorder)
 
 void	adaptive_sort(t_stack **stack_a, t_stack **stack_b, t_ctx *ctx)
 {
-	float 	disorder;
+	float	disorder;
 
 	if (!stack_a || !*stack_a)
 		return ;
-		
 	disorder = calculate_disorder(*stack_a);
 	set_adaptive_string(ctx, disorder);
-
 	if (handle_small_stacks(stack_a, stack_b, ctx))
 		return ;
 	if (disorder == 0.0)
