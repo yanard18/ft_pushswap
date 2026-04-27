95ff3d0e585855746425da34b3759a07ee407ba0
Deniz Doga Yanar <dyanar@k1m06s06.42istanbul.com.tr>
2026-04-27 19:17:06 +0300

    fix: norm sort_complex.c

--- src/strategies/sort_complex.c
+++ src/strategies/sort_complex.c
@@ -6,7 +6,7 @@
 /*   By: ekablan <ekablan@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/11 22:15:20 by dyanar            #+#    #+#             */
-/*   Updated: 2026/04/27 18:17:29 by ekablan          ###   ########.fr       */
+/*   Updated: 2026/04/27 19:13:08 by dyanar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,15 @@ static int	get_max_normalized_val(t_stack *stack)
 
 static int	get_bits_count(int value)
 {
-	int count = 0;
-    
-    while (value) 
-    {
-        count++; // Just add 1 for every bit position
-        value >>= 1;
-    }
-    return (count);
+	int	count;
+
+	count = 0;
+	while (value)
+	{
+		count++;
+		value >>= 1;
+	}
+	return (count);
 }
 
 static int	stack_size(t_stack *stack)
@@ -51,39 +52,30 @@ static int	stack_size(t_stack *stack)
 	return (size);
 }
 
-void complex_sort(t_stack **stack_a, t_stack **stack_b, t_ctx *ctx)
+void	complex_sort(t_stack **stack_a, t_stack **stack_b, t_ctx *ctx)
 {
-    int i;
-    int j;
-    int size;
-    int max_bits;
+	int	i;
+	int	j;
+	int	size;
+	int	max_bits;
 
-    if (!stack_a || !*stack_a)
-        return ;
-
-    size = stack_size(*stack_a);
-
-    if (handle_small_stacks(stack_a, stack_b, ctx))
-        return ;
-    else
-    {
-        stack_normalize(*stack_a);
-        max_bits = get_bits_count(get_max_normalized_val(*stack_a));
-        i = 0;
-        while (i < max_bits)
-        {
-            j = 0;
-            while (j < size)
-            {
-                if ((((*stack_a)->normalized_value >> i) & 1) == 0)
-                    pb(stack_a, stack_b, ctx);
-                else
-                    ra(stack_a, ctx, 1);
-                j++;
-            }
-            while (*stack_b != NULL)
-                pa(stack_a, stack_b, ctx);
-            i++;
-        }
-    }
+	if (handle_small_stacks(stack_a, stack_b, ctx))
+		return ;
+	size = stack_size(*stack_a);
+	stack_normalize(*stack_a);
+	max_bits = get_bits_count(get_max_normalized_val(*stack_a));
+	i = -1;
+	while (++i < max_bits)
+	{
+		j = -1;
+		while (++j < size)
+		{
+			if ((((*stack_a)->normalized_value >> i) & 1) == 0)
+				pb(stack_a, stack_b, ctx);
+			else
+				ra(stack_a, ctx, 1);
+		}
+		while (*stack_b)
+			pa(stack_a, stack_b, ctx);
+	}
 }
