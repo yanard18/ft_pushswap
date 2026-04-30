4578b1848384ef0e2c2284a12e4caa16104dfd01
Deniz Doga Yanar <dyanar@k1m02s07.42istanbul.com.tr>
2026-04-30 17:07:01 +0300

    fix: norm main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ekablan <ekablan@student.42istanbul.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/11 21:38:07 by dyanar            #+#    #+#             */
-/*   Updated: 2026/04/27 18:18:46 by ekablan          ###   ########.fr       */
+/*   Updated: 2026/04/30 17:06:47 by dyanar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,24 @@
 
 int	main(int argc, char **argv)
 {
-	t_ctx *ctx;
-	t_stack *stack_b;
+	t_ctx	*ctx;
+	t_stack	*stack_b;
 
 	stack_b = NULL;
-	
 	if (argc < 2)
 		return (1);
-
 	if (!is_input_valid(argc, argv))
 		return (ft_putstr_fd("Error\n", 2), 1);
-
 	ctx = parse(argc, argv);
 	if (ctx == NULL)
 		return (ft_putstr_fd("Error\n", 2), 1);
-
 	if (ctx->bench)
 		ctx->benchmark.disorder = calculate_disorder(ctx->stack);
-
 	if (ctx->sort)
 		ctx->sort(&(ctx->stack), &stack_b, ctx);
-
 	if (ctx->bench)
 		print_benchmark(ctx);
-
 	stack_clear(&ctx->stack);
 	free(ctx);
 	return (0);
 }
-
-//  ./push_swap --complex 1 5 2 4 3
-//  ./push_swap --bench --complex 3 1 2
